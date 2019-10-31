540aa62d9fe6583e5c228c1eec096253cfe2603c
fengye <1637364105@qq.com>
2019-10-31 14:00:11 +0800

    用户修改完成

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,43 +1,86 @@
 $(function() {
     //添加用户功能
     $('#userForm').on('submit', function() {
-            var formData = $(this).serialize()
-            $.ajax({
-                url: '/users',
-                type: 'post',
-                data: formData,
-                success: function(data) {
-                    // console.log(data);
-                    location.reload()
-                },
-                error: function() {
-                    console.log('创建用户失败');
-
-                }
-            })
-            return false
-        })
-        //上传头像设置
-    $('#avatar').on('change', function() {
-        var formData = new FormData()
-        formData.append('avatar', this.files[0])
-        console.log(this.files[0]);
-
+        var formData = $(this).serialize()
         $.ajax({
-            url: '/upload',
+            url: '/users',
             type: 'post',
             data: formData,
-            processData: false,
-            contentType: false,
             success: function(data) {
-                console.log(data);
-                $('#youphone').attr('src', data[0].avatar)
-                $('#hiddenAvatar').val(data[0].avatar)
-
+                // console.log(data);
+                location.reload()
             },
             error: function() {
-                alert('上传头像失败')
+                console.log('创建用户失败');
+
             }
         })
+        return false
+    })
+
+    //上传头像设置
+    $('#modifyBox').on('change', '#avatar', function() {
+            var formData = new FormData()
+            formData.append('avatar', this.files[0])
+            console.log(this.files[0]);
+
+            $.ajax({
+                url: '/upload',
+                type: 'post',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(data) {
+                    console.log(data);
+                    $('#youphone').attr('src', data[0].avatar)
+                    $('#hiddenAvatar').val(data[0].avatar)
+
+                },
+                error: function() {
+                    alert('上传头像失败')
+                }
+            })
+        })
+        //用户列表显示
+    $.ajax({
+            url: '/users',
+            type: 'get',
+            success: function(data) {
+                var html = template('tp1', {
+                    data: data
+                })
+                $('#tbodyBox').html(html)
+
+            }
+        })
+        //根据id查询用户
+    $('#tbodyBox').on('click', '.edit', function() {
+        var id = $(this).attr('data-id')
+        $.ajax({
+            url: `/users/${id}`,
+            type: 'get',
+            success: function(data) {
+                // console.log(data);
+                //将获取到的数据显示在左侧
+                var html = template('modifyTp2', data)
+                $('#modifyBox').html(html)
+            }
+        })
+    })
+    $('#modifyBox').on('submit', '#editForm', function() {
+        var params = $(this).serialize()
+        var id = $(this).attr('data-id')
+        $.ajax({
+            url: `/users/${id}`,
+            type: 'put',
+            data: params,
+            success: function(data) {
+                console.log(data);
+                location.reload()
+
+            }
+        })
+
+        return false
     })
 })
